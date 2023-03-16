9cd7484be8455a4ca1fa2640655a688b7ed77ec6
Net_Runner_13 <45355753+Jacob-PB@users.noreply.github.com>
2023-03-16 21:05:59 +0100

    Delete Code1.cpp

--- Find the stray number/Code1.cpp
+++
@@ -1,7 +0,0 @@
-int stray(std::vector<int> numbers) {
-  for(int i = 0; i <= numbers.size(); i++){
-    if((numbers[i-1] != numbers[i]) && (numbers[i] != numbers[i+1]))
-    return numbers[i];
-  }
-
-};
